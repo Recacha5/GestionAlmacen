bf1595cbf70a2449657d1e9368070b97a91aadab
alumno <alumno@aula01-14>
2018-01-11 17:38:09 +0100

    Comenzamos

--- GestionAlmacen/src/gestionalmacen/GestionAlmacen.java
+++ GestionAlmacen/src/gestionalmacen/GestionAlmacen.java
@@ -5,6 +5,9 @@
  */
 package gestionalmacen;
 
+import Menus.Menu;
+import java.util.Scanner;
+
 /**
  *
  * @author Alex Recacha
@@ -12,9 +15,43 @@ package gestionalmacen;
 public class GestionAlmacen {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        boolean bandera = true;
+        int fila = 0, columna = 0;
         
         
+        System.out.println("Dime las dimensiones de tu estantería");
+        System.out.print("Filas: ");
+        fila = sc.nextInt();
+        System.out.print("Columnas: ");
+        columna = sc.nextInt();
         
+        Almacen almacen = new Almacen(fila, columna);
+        
+        while (bandera){
+            Scanner leer = new Scanner (System.in);
+            switch(Menu.pintaMenu()){
+
+                case 1: almacen.precioTotal();
+                        
+                        break;
+                case 2: System.out.println("Dime la marca.");
+                        almacen.precioTotalMarca(leer.nextLine());
+                        break;
+                case 3: almacen.precioTotalColumna(leer.nextInt());
+                        break;
+                case 4: 
+                        break;
+                case 5:
+                        break;
+                case 6: 
+                        break;
+                case 7: bandera = false;
+                        break;
+                
+
+            }
+        }
     }
     
 }
